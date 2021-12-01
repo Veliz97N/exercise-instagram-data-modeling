db8714ad16ae29f9c54aa970e7873c67b9c4f612
Veliz97N <nicolas.veliz@usach.cl>
2021-12-01 00:51:42 +0000

    Se termina codigo

--- src/models.py
+++ src/models.py
@@ -1,20 +1,62 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Text, DateTime,Table
 from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy import render_er
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = 'users'    
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    last_name = Column(String(250), nullable=False)
-    password= Column(String(250), nullable=False)
+historia_visto = Table('historia_visto', Base.metadata,
+    Column('id_usuario', ForeignKey('users.id_user'), nullable=False),
+    Column('id_historia', ForeignKey('historias.id_historia'), nullable=False)
+)
 
+class User(Base):
+    __tablename__ = 'users'
+    id_user = Column(String(50), primary_key=True)
+    email = Column(String(50), nullable=False, unique=True)
+    descripcion = Column(Text)
+    name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    nickname = Column(String(50), nullable=False)
+    created_at = Column(DateTime(), default=datetime.now())
+
+class Publicacion(Base):
+    __tablename__ = 'publicaciones'
+    id_publicaciones = Column(Integer(), primary_key=True)
+    contenido = Column(String(250))
+    descripcion = Column(Text())
+    created_at = Column(DateTime(), default=datetime.now())
+    ubicacion = Column(String(150))
+    id_usuario = Column(String(50), ForeignKey('users.id_user'), nullable=False)
+
+class Comentario(Base):
+    __tablename__ = 'comentarios'
+    id_comentario = Column(Integer(), primary_key=True)
+    id_usuario = Column(String(50), ForeignKey('users.id_user'), nullable=False)
+    contenido = Column(Text())
+
+class Like(Base):
+    __tablename__ = 'likes'
+    usuarios = Column(String(50), ForeignKey('users.id_user'), primary_key=True)
+    id_comentario = Column(Integer(), ForeignKey('comentarios.id_comentario'), primary_key=True)
+    id_publicaciones = Column(Integer(), ForeignKey('publicaciones.id_publicaciones'), primary_key=True)
+
+class Historia(Base):
+    __tablename__ = 'historias'
+    id_historia = Column(Integer, primary_key=True)
+    id_usuario = Column(String(50), ForeignKey('users.id_user'), nullable=False)
+    media = Column(String(250), nullable=False)
+    created_at = Column(DateTime(), default=datetime.now())
+    historia_visto= relationship("User",secondary=historia_visto)
+
+# class Visto(Base):
+#     __tablename__ = 'vistos'
+#     usuario = Column(String(50), ForeignKey('users.user_id'), primary_key=True)
+#     historia = Column(Integer(), ForeignKey('historias.id_historias'), primary_key=True)
 
 
 ## Draw from SQLAlchemy base
